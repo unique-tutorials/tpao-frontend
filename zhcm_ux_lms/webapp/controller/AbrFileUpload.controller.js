c2f88192ab6b929fb10094a131fc1c2c84b90d44
Nurgül Özçilingir <128186112+unique-tutorials@users.noreply.github.com>
2024-11-10 13:13:09 +0300

    fixed

--- zhcm_ux_lms/webapp/controller/AbrFileUpload.controller.js
+++ zhcm_ux_lms/webapp/controller/AbrFileUpload.controller.js
@@ -15,19 +15,20 @@ sap.ui.define([
         formatter: formatter,
         onInit: function () {
             var oViewModel = new JSONModel();
-            this.setModel(oViewModel, "requestListModel");
+            this.setModel(oViewModel, "wageRequestListModel");
             this._initiateModel();
-            this.getRouter().getRoute("AbrTracking").attachPatternMatched(this._onRequestListMatched, this);
+            this.getRouter().getRoute("AbrFileUpload").attachPatternMatched(this._onRequestListMatched, this);
         },
         _onRequestListMatched: function (oEvent) {
             this._getRequestList();
         },
         _initiateModel: function () {
-            var oViewModel = this.getModel("requestListModel");
+            var oViewModel = this.getModel("wageRequestListModel");
             oViewModel.setData({
                 requestList: [],
                 selectedRequest: {},
-                currentRequest: {}
+                currentRequest: {},
+                wageSearchRequest:{}
              
             });
         },
@@ -45,6 +46,41 @@ sap.ui.define([
             if (this._oNewWageSearchHelpDialog) {
                 this._oNewWageSearchHelpDialog.close();
             }
-         }
+         },
+       
+         onSearch: function(oEvent) {
+            debugger;
+            var oViewModel = this.getModel('wageRequestListModel');
+            var oFilter = oViewModel.getProperty('/wageSearchRequest');
+
+            // var sDateValue = oFilter.Wagpe;
+            // if (sDateValue) {
+
+            //     var dateFormat = sap.ui.core.format.DateFormat.getDateInstance({ pattern: "MM-y" }).parse(sDateValue);
+            //     var sFormattedDate = sap.ui.core.format.DateFormat.getDateInstance({ pattern: "yyyymm" }).format(dateFormat);
+            //     oFilter.Wagpe = sFormattedDate;
+            // }
+        
+            oFilter.Prope = "1";
+        
+            var aFilters = this._getFilters(oFilter);
+        
+            var oTable = this.getView().byId('idSalariesSetTable') || sap.ui.getCore().byId('idSalariesSetTable');
+            oTable.getBinding('items').filter(aFilters, "Application");
+        },
+        
+        _getFilters: function(oFilter) {
+            var aFilters = [];
+            var aKeys = Object.keys(oFilter);
+            for (var i = 0; i < aKeys.length; i++) {
+                var sVal = oFilter[aKeys[i]].toString();
+                if (sVal) {
+                    var oFilterElement = new Filter(aKeys[i], FilterOperator.EQ, sVal);
+                    aFilters.push(oFilterElement);
+                }
+            }
+            return aFilters;
+        }
+            
 	});
 });
